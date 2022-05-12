c10da57817d85289e56d77fa28d2568a8fd8347f
Dylan <dylblob@gmail.com>
2022-05-12 00:07:37 -0400

    Start using DOM Manipulation methods To call the elements and buttons in the index.html file, so they can be used by the operators.

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
-let a = prompt("please choose a number ");
-let b = prompt("please choose a number ");
-let num1 = parseFloat(a)
-let num2 = parseFloat(b)
+let presentNum ="";
+let priorNum ="";
+let operator ="";
 
-add(num1, num2)
-subtract(num1, num2) 
-multiply(num1, num2) 
-divide(num1, num2)
+const presentNumberDisplay = document.querySelector('.presentNumber');
+const priorNumberDisplay = document.querySelector('.priorNumber');
+const equal = document.querySelector('.equal');
+const decimal = document.querySelector('.decimal');
+const clearDisplay = document.querySelector('.clear');
+const buttonNumber = document.querySelectorAll('.number');
+const fourOperator = document.querySelectorAll('.operator');
 
+buttonNumber.forEach((btn) => {
+    btn.addEventListener('click', (e) =>{
+        getNumber(e.target.textContent)
+    });
+});
 
-function add(num1, num2){
- let valueAdd = (num1 +num2);
- console.log("valueAdd", valueAdd)
+function getNumber(number){
+    console.log(number);
 }
 
-function subtract(num1, num2){
-    let valueSubtract = (num1 - num2);
-    console.log("valueSubtract", valueSubtract)
-   }
 
- function multiply(num1, num2){
-    let valueMultiply = (num1*num2);
-    console.log("valueMultiply", valueMultiply)
-   }
 
-   function divide(num1, num2){
-    let valueDivide = (num1/num2);
-    console.log("valueDivide", valueDivide)
-   }
+//let a = prompt("please choose a number ");
+//let b = prompt("please choose a number ");
+//let num1 = parseFloat(a)
+//let num2 = parseFloat(b)
+
+
+//add(num1, num2)
+//subtract(num1, num2) 
+//multiply(num1, num2) 
+//divide(num1, num2)
+
+
+//function add(num1, num2){
+// let valueAdd = (num1 +num2);
+ //console.log("valueAdd", valueAdd)
+//}
+
+//function subtract(num1, num2){
+//    let valueSubtract = (num1 - num2);
+//    console.log("valueSubtract", valueSubtract)
+//   }
+
+ //function multiply(num1, num2){
+ //   let valueMultiply = (num1*num2);
+  //  console.log("valueMultiply", valueMultiply)
+ //  }
+
+  // function divide(num1, num2){
+  //  let valueDivide = (num1/num2);
+  //  console.log("valueDivide", valueDivide)
+   
+
+  // if (valueDivide == "Infinity"){
+  //     return alert ("You can't divide by zero!")
+ //  } else return console.log("valueDivide", valueDivide);
+//}
 
