f946d366f2e60b940b7e5036b3e9b6f50400741c
DylanL101 <103328750+DylanL101@users.noreply.github.com>
2022-05-12 18:12:42 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -109,44 +109,7 @@ function useDecimal(){
         presentNumberDisplay.textContent = presentNum;
     }
    
-}
+};
 
 
-
-//let a = prompt("please choose a number ");
-//let b = prompt("please choose a number ");
-//let num1 = parseFloat(a)
-//let num2 = parseFloat(b)
-
-
-//add(num1, num2)
-//subtract(num1, num2) 
-//multiply(num1, num2) 
-//divide(num1, num2)
-
-
-//function add(num1, num2){
-// let valueAdd = (num1 +num2);
- //console.log("valueAdd", valueAdd)
-//}
-
-//function subtract(num1, num2){
-//    let valueSubtract = (num1 - num2);
-//    console.log("valueSubtract", valueSubtract)
-//   }
-
- //function multiply(num1, num2){
- //   let valueMultiply = (num1*num2);
-  //  console.log("valueMultiply", valueMultiply)
- //  }
-
-  // function divide(num1, num2){
-  //  let valueDivide = (num1/num2);
-  //  console.log("valueDivide", valueDivide)
-   
-
-  // if (valueDivide == "Infinity"){
-  //     return alert ("You can't divide by zero!")
- //  } else return console.log("valueDivide", valueDivide);
-//}
 
